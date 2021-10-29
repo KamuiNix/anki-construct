e7470ef396b741cfe207ac88e519664bb6590883
Kamui <fin-kamui@pm.me>
2021-10-28 21:10:02 -0500

    fix: night mode css

--- script.js
+++ script.js
@@ -93,12 +93,10 @@ class Constructor {
     }
 }
 
-// Mock data
-const before = "なかなか";
-const after = "行けることになった。";
-const correct =
-    "なかなか予約が取れなくて行けなかったレストランにようやく行けることになった。";
-const bankUnsplitted = "予約が取れなくて 行けなかった レストランに ようやく";
+const before = "{{Before}}";
+const after = "{{After}}";
+const correct = "{{Correct}}";
+const bankUnsplitted = "{{Options}}";
 const bankSplitted = bankUnsplitted.split(" ");
 
 const constructor = new Constructor(before, after, bankSplitted, correct);
@@ -111,19 +109,32 @@ const appendElement = (parent, elm) => {
     }
 };
 
-const outputModel = (model) => {
-    beforeSpan.innerHTML = model.before;
-    afterSpan.innerHTML = model.after;
+const paintGuesses = () => {
+    guessDiv.innerHTML = "";
+    constructor.guessWords.forEach((word) => {
+        const element = document.createElement(
+            word == EMPTY ? "div" : "button"
+        );
+        element.className = word === EMPTY ? "empty-word" : "word-btn";
+        element.appendChild(document.createTextNode(word));
+        guessDiv.append(element);
+    });
+    const guessBtns = guessDiv.querySelectorAll(".word-btn");
+    setupPopPushEvents(guessBtns);
+};
 
+const paintBank = () => {
+    bankDiv.innerHTML = "";
     constructor.bank.forEach((word) => {
         const element = document.createElement("button");
+        element.className = "word-btn";
         element.appendChild(document.createTextNode(word));
         bankDiv.append(element);
     });
+};
 
-    // Setup click events on bank buttons
-    const bankBtns = bankDiv.querySelectorAll("button");
-    bankBtns.forEach((btn) => {
+const setupPopPushEvents = (buttons) => {
+    buttons.forEach((btn) => {
         btn.onclick = (ev) => {
             const word = ev.target.textContent;
             if (ev.target.parentNode.className === "guesses") {
@@ -135,8 +146,24 @@ const outputModel = (model) => {
                 constructor.insert(word);
                 appendElement(guessDiv, btn);
             }
+            paintGuesses();
         };
     });
+};
+
+const setupEvents = () => {
+    const btns = document.querySelectorAll(".word-btn");
+    setupPopPushEvents(btns);
+};
+
+const outputModel = (model) => {
+    beforeSpan.innerHTML = model.before;
+    afterSpan.innerHTML = model.after;
+
+    paintGuesses();
+    paintBank();
+
+    setupEvents();
 };
 
 resetBtn.onclick = (ev) => {
@@ -148,14 +175,18 @@ resetBtn.onclick = (ev) => {
     });
     constructor.shuffle();
 
-    // delete all bank nodes in DOM temporarily
-    bankDiv.innerHTML = "";
-
     outputModel(constructor);
 };
 
 checkBtn.onclick = (ev) => {
     const isCorrect = constructor.check();
+    if (isCorrect) {
+        analysisDiv.style.color = "#b8bb26";
+        analysisDiv.innerHTML = "Correct!";
+    } else {
+        analysisDiv.style.color = "#ca4e4e";
+        analysisDiv.innerHTML = "Incorrect";
+    }
     analysisDiv.innerHTML = isCorrect ? "Correct!" : "Incorrect";
 };
 
